70c67cb1e2e504531b7f21204ef21ef1bf910048
Zhao Zhang <zhangzhao0718@gmail.com>
2015-09-23 16:00:38 -0700

    restore to Zhao's change

--- ganglia/init.sh
+++ ganglia/init.sh
@@ -11,16 +11,20 @@ chown -R nobody:nobody /mnt/ganglia/rrds
 # Install ganglia
 # TODO: Remove this once the AMI has ganglia by default
 
+yum -y remove httpd
+yum remove php-cli* php-common* httpd-tools* php-process* php-xml*
+
 GANGLIA_PACKAGES="ganglia ganglia-web ganglia-gmond ganglia-gmetad"
 
-if ! rpm --quiet -q $GANGLIA_PACKAGES; then
-  yum install -q -y $GANGLIA_PACKAGES;
-fi
+yum install -q -y $GANGLIA_PACKAGES;
+
 for node in $SLAVES $OTHER_MASTERS; do
-  ssh -t -t $SSH_OPTS root@$node "if ! rpm --quiet -q $GANGLIA_PACKAGES; then yum install -q -y $GANGLIA_PACKAGES; fi" & sleep 0.3
+  ssh -t -t $SSH_OPTS root@$node "yum install -q -y $GANGLIA_PACKAGES" & sleep 0.3
 done
 wait
 
 # Post-package installation : Symlink /var/lib/ganglia/rrds to /mnt/ganglia/rrds
 rmdir /var/lib/ganglia/rrds
 ln -s /mnt/ganglia/rrds /var/lib/ganglia/rrds
+
+sed -i 's/Listen 80/Listen 5080/g' /etc/httpd/conf/httpd.conf
