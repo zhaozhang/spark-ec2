08f5c9fb424988717767b1c24f3166c8975f9664
Zhao Zhang <zhangzhao0718@gmail.com>
2016-01-25 10:52:34 -0800

    trying mr1 client library

--- spark/init.sh
+++ spark/init.sh
@@ -15,7 +15,7 @@ git_hash="streaming-pyKira"
 git remote add origin $repo
 git fetch origin
 git checkout $git_hash
-build/mvn -Pyarn -Phadoop-2.4 -Dhadoop.version=2.4.0 -DskipTests clean package
+build/mvn -Dhadoop.version=2.0.0-mr1-cdh4.2.0 -Phadoop-1 -DskipTests clean package
 #sbt/sbt clean assembly
 #sbt/sbt publish-local
 popd > /dev/null
